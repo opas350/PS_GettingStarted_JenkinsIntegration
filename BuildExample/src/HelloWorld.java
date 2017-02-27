1cece0c505130fd27da3d407ea101c449af4cbf9
Reyes Valdez <javier.reyes.valdez@accenture.com>
2017-02-27 02:24:15 -0600

    remove unused files

--- BuildExample/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by javier.reyes.valdez on 2/26/2017.
- */
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
-    }
-}
